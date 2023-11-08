b5a72f626cef7fc1826ea004daef2d3c9f77c4e2
Ezekiel <ezekieljamolin186@gmail.com>
2023-11-07 17:02:57 -0800

    blogs showing up on home page

---
+++ controllers/api/comment-route.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const { Comments } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const commentData = await Comments.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const commentData = await Comments.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
+    }
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const commentData = await Comments.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
+    }
+
+    const comment = commentData.get({plain: true});
+
+    res.render('comment', {comment});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
+const {User, Posts }= require('../models');
+
+router.get('/', async (req, res)=> {
+    const dbPosts= await Posts.findAll();
+    const posts = dbPosts.map((post) => post.get({plain: true}));
+    res.render('homepage', {posts} );
+});
 
 router.get('/login', async (req, res) => {
   res.render('login');
@@ -16,4 +23,9 @@ router.get('/', (req, res) => {
   res.render('homepage');
 });
 
+router.get('*',async (req, res)=> {
+  const dbPosts = await Posts.findAll();
+  const posts = dbPosts.map((post) => post.get({plain: true}));
+  res.render('homepage', {posts} );
+})
 module.exports = router;
